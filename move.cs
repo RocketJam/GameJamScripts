3f0106601b95a128259f077d5cec1071445d7f55
RocketJam <rocketjam3@gmail.com>
2020-05-15 02:07:48 -0700

    Add files via upload

---
+++ move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Valve.VR.InteractionSystem.Hand.CustomStart() == true){
+            transform.position += Time.deltaTime * new Vector3(0, 0, 4);
+        }
+
+        
+    }
+}
